2bfc34b2cb700d2c03d8befc13f26f5417263dc2
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-07-10 12:00:25 +0530

    largest of two numbers

---
+++ big.java
@@ -0,0 +1,15 @@
+public class big
+{
+	public static void main(String[] args)
+	{
+		int a=50,b=10;
+		if(a==b)
+			System.out.println("Both are equal");
+		else if(a>b)
+			System.out.println(a+" is greater");
+		else
+			System.out.println(b+" is greater");
+	}
+}
+
+
