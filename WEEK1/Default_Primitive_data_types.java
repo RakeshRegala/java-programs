906c02e2d805684e12c0c4f152cfad576ef1f25b
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-07-24 12:20:47 +0530

    Default_Primitive_data_types.java

---
+++ WEEK1/Default_Primitive_data_types.java
@@ -0,0 +1,22 @@
+
+public class primitive{
+    static byte Byte;
+    static short Short;
+    static int Int;
+    static float Float;
+    static long Long;
+    static double Double;
+    static char Char;
+    static boolean Boolean;
+    public static void main(String[] args){
+        System.out.println("The   values of primitive data types in java are:");
+        System.out.println("byte:"+  Byte);
+        System.out.println("short:"+  Short);
+        System.out.println("int:"+  Int);
+        System.out.println("float:"+  Float);
+        System.out.println("long:"+  Long);
+        System.out.println("double:"+  Double);
+        System.out.println("char:"+  Char);
+        System.out.println("boolean:"+  Boolean);
+    }
+}
