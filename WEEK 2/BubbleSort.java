a3c39a851bf1370652f249009210194a87a45073
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-08-01 10:39:14 +0530

    Update BubbleSort.java

--- WEEK 2/BubbleSort.java
+++ WEEK 2/BubbleSort.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class sort{
+class sorting{
 	void BubbleSort(int a[],int n){
 	   int i,j,temp;
 		for(i=0;i<=n-1;i++)
@@ -13,12 +13,14 @@ public class sort{
 				}
 			}
 	}
-	
+}
+	public class sort{
+
 	public static void main(String[] args){
 	
 	Scanner sc=new Scanner(System.in);
 		int i,n;
-		sort obj=new sort();
+		sorting obj=new sorting();   //object must be created for class name in which method is written
 		System.out.println("Enter number of elements: ");
 		n=sc.nextInt();
 		int arr[]=new int[n];
@@ -31,4 +33,5 @@ public class sort{
     		for(i=0; i<n; i++)
         	System.out.print(arr[i]+" ");
 	}
-	}
+   
+    }
