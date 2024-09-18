b9318ffd67ed6517b8b9828bed831b0b4fd9ea82
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-09-18 11:41:32 +0530

    Update Single Inheritance Person.java

--- WEEK 04/Single Inheritance Person.java
+++ WEEK 04/Single Inheritance Person.java
@@ -9,6 +9,7 @@ class Person{
     }
 
     void display(){
+        System.out.println("FROM PERSON(super class)CLASS");
         System.out.println("Name is:"+name); 
         System.err.println("Age is:"+age);
     }
@@ -24,6 +25,7 @@ class Employee extends Person{
     }
   //  @Override
     void display(){
+        System.out.println("FROM EMPLOYEE(sub class)CLASS");
         System.out.println("Name is: "+name); 
         System.err.println("Age is: "+age);
         System.out.println("Salary is: "+salary);
@@ -33,8 +35,10 @@ class Employee extends Person{
 
 public class inheritance{
     public static void main(String[] args){
+        Person p=new Person("Shiva",20);
         Employee obj=new Employee("Rakesh",23,2000000);
-
+        p.display();
+        System.out.println();
         obj.display();
     }
 }
