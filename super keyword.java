e6a546f29afccdcf3951da3821495f552ce99e56
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-09-25 11:12:24 +0530

    Create super keyword.java

---
+++ super keyword.java
@@ -0,0 +1,42 @@
+//import java.util.*;
+class Person{
+    String name;
+    int age;
+
+    Person(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+
+    void display(){
+        System.out.println("FROM PERSON(super class)CLASS");
+        System.out.println("Name is:"+name); 
+        System.err.println("Age is:"+age);
+    }
+}
+
+class Employee extends Person{
+    double salary;
+
+    Employee(String name,int age,double salary){
+        super(name,age);                                                                                             
+        this.salary=salary;
+    }
+  //  @Override
+    void display(){
+        System.out.println("FROM EMPLOYEE(sub class)CLASS");
+        super.display();
+        System.out.println("Salary is: "+salary);
+
+    }
+}
+
+public class inheritance{
+    public static void main(String[] args){
+        Person p=new Person("Shiva",20);
+        Employee obj=new Employee("Rakesh",23,2000000);
+        p.display();
+        System.out.println();
+        obj.display();
+    }
+}
