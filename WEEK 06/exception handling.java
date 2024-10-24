f7b34a90c967a1660984ace928a4926cfc5c214f
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-10-24 21:49:51 +0530

    Create exception handling.java

---
+++ WEEK 06/exception handling.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Handling{
+	void divide(int a,int b){
+		try{
+			System.out.println(a/b);
+		}
+		catch(ArithmeticException e){
+			System.out.println(e.toString());
+			System.out.println("Error! Division by Zero is not allowed");
+		}
+		finally{
+			System.out.println("Division is competed");
+		}
+}
+}
+
+public class exception{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int a,b;
+		System.out.println("Enter Two numbers: ");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		Handling obj=new Handling();
+
+		obj.divide(a,b);
+	}
+}
+
+
