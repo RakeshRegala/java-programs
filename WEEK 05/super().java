c8ffd4fc907e93c48893f34f33e6cb07430db55f
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-10-24 19:46:20 +0530

    Create super().java

---
+++ WEEK 05/super().java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Animal{
+    String name;
+    Animal(){
+        
+        System.out.println("Animal constructor is called");
+    }
+    void sound(){
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal{
+    Dog(){
+        super();
+        System.out.println("Dog constructor is called");
+    }
+    void Sound(){
+        super.sound();
+        System.out.println("Animal makes sound");
+    }
+}
+
+public class hello{
+    public static void main(String[] args){
+        Dog in=new Dog();
+        in.Sound();
+    }
+}
